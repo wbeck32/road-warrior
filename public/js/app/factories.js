dd94c6042795d8ef33e5b13f439a010286688f02
Russell DuBrock <russell.dubrock@gmail.com>
2015-03-18 16:55:01 -0700

    over to wendy

--- public/js/app/factories.js
+++ public/js/app/factories.js
@@ -223,7 +223,7 @@ angular.module('roadWarrior').factory('elevationProfileFactory', function(){
     document.getElementById('elevation-chart').style.display = 'block';
     chart.draw(data, {
       legend: 'none',
-      titleY: 'Elevation (m)'
+      titleY: 'none'
   });
   }
 })
