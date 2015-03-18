9cc6153cf28b73d1ea5d83477267923a3f82321e
BryceJohnson <bj4242@gmail.com>
2015-03-17 18:53:51 -0700

    added filters for displaying distance and elevation

---
+++ public/js/app/filters.js
@@ -0,0 +1,15 @@
+angular.module('roadWarrior').filter('totalDistance', function(){
+  return function(input){
+    return input.reduce(function(prev, leg, index, array){
+      return prev + leg.rend.directions.routes[0].legs[0].distance.value;
+    }, 0);
+  };
+}).filter('metersToMiles', function(){
+  return function(input){
+    return (input * .0006214).toFixed(2);
+  };
+}).filter('metersToFeet', function(){
+  return function(input){
+    return (input * 3.281).toFixed(0);
+  };
+});
