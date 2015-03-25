8a041ad98671425200a2e10ae604257b3fd401fe
Wendy Beck <webeck@gmail.com>
2015-03-25 16:16:17 -0700

    rejiggering side menu to fly out with tabs

--- public/js/app/functions.js
+++ public/js/app/functions.js
@@ -1,4 +1,56 @@
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+$(document).ready(function(){
+
+$('.tab').click(function() {
+	$('.sidebarContent').addClass('sidebarLeft');
+});
+
+$('.trekListTab').click(function() {
+	if ($(this).hasClass('blueBack')){
+		$(this).removeClass('blueBack');
+		$('.tabList').removeClass('slideLeft');
+		$('.sidebarContent').removeClass('sidebarLeft');
+	} else {
+		$('.tabList').addClass('slideLeft');
+		$(this).addClass('blueBack');
+		$('.currentTab').removeClass('blueBack');
+		$('.directionsTab').removeClass('blueBack');
+		$('.trekListPanel').addClass('visible');
+		$('.currentPanel').removeClass('visible');
+		$('.directionsPanel').removeClass('visible');
+	}
+});
+
+$('.currentTab').click(function() {
+	if ($(this).hasClass('blueBack')){
+		$(this).removeClass('blueBack');
+		$('.tabList').removeClass('slideLeft');
+		$('.sidebarContent').removeClass('sidebarLeft');
+	} else {
+		$('.tabList').addClass('slideLeft');
+		$(this).addClass('blueBack');
+		$('.trekListTab').removeClass('blueBack');
+		$('.directionsTab').removeClass('blueBack');
+		$('.trekListPanel').removeClass('visible');
+		$('.currentPanel').addClass('visible');
+		$('.directionsPanel').removeClass('visible');
+	}
+});
+
+$('.directionsTab').click(function() {
+	if ($(this).hasClass('blueBack')){
+		$(this).removeClass('blueBack');
+		$('.tabList').removeClass('slideLeft');
+		$('.sidebarContent').removeClass('sidebarLeft');
+	} else {
+		$('.tabList').addClass('slideLeft');
+		$(this).addClass('blueBack');
+		$('.currentTab').removeClass('blueBack');
+		$('.trekListTab').removeClass('blueBack');
+		$('.trekListPanel').removeClass('visible');
+		$('.currentPanel').removeClass('visible');
+		$('.directionsPanel').addClass('visible');
+	}
+});
+
+});
+
