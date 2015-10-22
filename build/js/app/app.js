9b6b325f2e0a07cec4d59c5e148186b8a902c98c
brycewjohnson <bj4242@gmail.com>
2015-10-21 21:02:39 -0700

    angular production tweaks - disable debug and enable strict dependency injection

--- build/js/app/app.js
+++ build/js/app/app.js
@@ -1,3 +1,6 @@
 // this is app.js - main client side angular file
 
 var roadWarrior = angular.module('roadWarrior', ['ngCookies']);
+roadWarrior.config(['$compileProvider', function ($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}]);
